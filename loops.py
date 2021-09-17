02949efb865e2ab4175f0c9594028f74112db495
Lara Mol <89733492+LaraMol@users.noreply.github.com>
2021-09-17 11:37:00 +0200

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,23 @@
+for i in range(1,11): #Eerste vraag
+    print (i * 4)
+
+print("\n")
+print ("===-===")
+
+for raketlancering in reversed(range(1,31)): #2e opdracht
+    print (raketlancering)
+
+print ("===-===")
+
+for time in range (0,24): #derde opdracht
+    if time <= (12):
+        print (str(time) + ":00 AM")
+    elif time > (12):
+        print (str(time) + ":00  PM")
+    
+print ("===-===")
+
+for i in range (20 , 50 , 2): #vierde opdracht
+    print (i)
+
+
